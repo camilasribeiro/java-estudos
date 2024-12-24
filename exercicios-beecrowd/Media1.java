ad58872d3e649b62d8b1b22b74331d7109b8978a
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-12-24 20:04:33 -0300

    Cálculo da média ponderada de duas notas

---
+++ exercicios-beecrowd/Media1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Media1 {
+    public static void main(String [] args) {
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double media = ((a * 3.5) + (b * 7.5)) / 11;
+
+        System.out.printf("MEDIA = %.5f%n", media);
+        scanner.close();
+    }
+}
