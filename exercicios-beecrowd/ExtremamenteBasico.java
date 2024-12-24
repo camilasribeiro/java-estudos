7b514ac6239c3c1646cf262649802aea4b1331fb
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-12-24 19:50:32 -0300

    Soma de 2 valores

---
+++ exercicios-beecrowd/ExtremamenteBasico.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class ExtremamenteBasico {
+	public static void main (String[] args) {
+		Scanner scanner = new Scanner (System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int x = a + b;
+		System.out.println("X = " + x);
+	}
+}
