133f5d24007986ed7c3cde28aac4c8123411d940
Camila Ribeiro <camilas.ribeiro@outlook.com>
2024-12-24 20:04:59 -0300

    Cálculo da média ponderada de três notas

---
+++ exercicios-beecrowd/Media2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Media2 {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        double media = ((a * 2) + (b * 3) + (c * 5)) / 10;
+
+        System.out.printf("MEDIA = %.1f%n", media);
+        scanner.close();
+    }
+}
