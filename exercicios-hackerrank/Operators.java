8e67d7c564d1c873cc7c20c8b22c33f5415e561f
Camila Ribeiro <camilas.ribeiro@outlook.com>
2025-03-02 18:09:43 -0300

    ChallengeDay2 - HackerRank

---
+++ exercicios-hackerrank/Operators.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Operators {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double mealCost = scanner.nextDouble();
+        int tipPercent = scanner.nextInt();
+        int taxPercent = scanner.nextInt();
+
+
+        double tip = mealCost * ((double) tipPercent / 100);
+        double tax = mealCost * ((double) taxPercent / 100);
+        double totalCost = mealCost + tip + tax;
+
+        System.out.println((int) Math.round(totalCost));
+
+    }
+}
