4d4ee3be9b86f86498898c0d511926b2ed998304
Camila Ribeiro <camilas.ribeiro@outlook.com>
2025-07-06 19:47:03 -0300

    ChallengeDay4 - HackerRank

---
+++ exercicios-hackerrank/JavaSubstring.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class JavaSubstring {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String S = scanner.next();
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        System.out.println(S.substring(start, end));
+    }
+}
