d69c6e73a05088d34c9b8b6986f8d9e81dfe74f0
Camila Ribeiro <camilas.ribeiro@outlook.com>
2026-06-06 22:21:04 -0300

    Controle de Senha

---
+++ java-alura/06-java-encapsulamento/src/UsuarioMain.java
@@ -0,0 +1,6 @@
+public class UsuarioMain {
+    public static void main(String[] args) {
+        Usuario user = new Usuario("cami","123456");
+        user.setSenhaNova("1234s56", "abc123");
+    }
+}
