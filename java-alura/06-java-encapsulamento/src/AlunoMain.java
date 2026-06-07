d51cb2afc9348ff7ba606d940a3e4a8cc46afc3b
Camila Ribeiro <camilas.ribeiro@outlook.com>
2026-06-06 22:28:35 -0300

    Sistema de Pontos para Alunos

---
+++ java-alura/06-java-encapsulamento/src/AlunoMain.java
@@ -0,0 +1,7 @@
+public class AlunoMain {
+    public static void main(String[] args) {
+        Aluno aluno = new Aluno("Fernanda");
+        aluno.ganharPontos(120);
+        aluno.exibirStatus();
+    }
+}
